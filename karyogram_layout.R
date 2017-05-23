c1ae23f582b0b53245b6f4a5aeaefecd97ec7aa2
Sangkon, Han <sigmadream@gmail.com>
2017-05-23 11:34:19 +0900

    Add Comments Install devtools and github install

--- karyogram_layout.R
+++ karyogram_layout.R
@@ -1,8 +1,13 @@
+## library(devtools)
+## install_github(genomicsclass/ERBS)
 library(ERBS)
+
 data(HepG2)
-library(GenomeInfoDb)  # trim all but autosomal chroms
+library(GenomeInfoDb)
 seqlevels(HepG2, force=TRUE) = paste0("chr", 1:22)
+
 data(GM12878)
 seqlevels(GM12878, force=TRUE) = paste0("chr", 1:22)
+
 library(ggbio)
 autoplot(HepG2, layout="karyogram", main="ESRRA binding on HepG2")
